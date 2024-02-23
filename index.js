578a77052c093df0113c005c693747c1da40198e
shaineblack <shaineblck@gmail.com>
2024-02-23 04:21:05 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    renderBooks(json);
+  })
+  .catch((error) => {
+    console.error("Error fetching books:", error);
+  });
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
